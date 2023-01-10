401dea8b8a2c6c187607a680ef8d8220d81ac916
Andrey Kramskoy <92092067+AndreyKramskoy@users.noreply.github.com>
2023-01-10 19:53:22 +0100

    Add files via upload

---
+++ wdio/test/specs/test.spec.js
@@ -0,0 +1,10 @@
+
+describe("Test suite", () => {
+    
+    it("First test", async () => {
+        await browser.url("https://ej2.syncfusion.com/showcase/angular/appointmentplanner/#/dashboard");
+        const pageTitle = await browser.getTitle();
+
+        console.log(pageTitle);
+    });
+})
