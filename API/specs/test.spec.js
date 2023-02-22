aceab228f927175fec5bd54d081c7d235e3a8886
Andrei Kramski <Andrei_Kramski@epam.com>
2023-02-22 18:04:27 +0100

    2

--- API/specs/test.spec.js
+++ API/specs/test.spec.js
@@ -3,42 +3,60 @@ const {
 } = require("../helpers/api.helper.js");
 const testData = require("../config/data.json")
 
-describe("API Test Suit", () => {
-
-    it("GET post/1", async ()=> {
-const response = await sendRequest("posts/1");
-
-console.log(response.data);
-console.log(response.status);
-
-expect(response.data.userId).to.equal(1);
-expect(response.status).to.equal(200);
-});
+describe("API Test Suit", () => {    
 
     it("POST post", async ()=> {
-        const response = await sendRequest("posts", {
-                title: 'Hello',
-                body: 'bar',
-                userId: 1,
-              }, "post");
+      const response = await sendRequest("posts", {
+      title: 'Hello',
+      body: 'bar',
+      userId: 1,
+      }, "post");
 
-              console.log(response.data);
-              console.log(response.status);
+      console.log(response.data);
+      console.log(response.status);
       
-              expect(response.status).to.equal(201);     
-        });
+      expect(response.status).to.equal(201);     
+      });
         
-
-
-it("PUT post", async ()=> {
-  const response = await sendRequest("posts", testData, "put");
-
+        it("GET post/1", async ()=> {
+          const response = await sendRequest("posts/1");
+          
           console.log(response.data);
           console.log(response.status);
+          
+          expect(response.data.userId).to.equal(1);
+          expect(response.status).to.equal(200);
+          });
+
+            it("PUT post", async ()=> {
+            const response = await sendRequest("posts/1", testData, "put");
+
+            console.log(response.data);
+            console.log(response.status);
       
-          expect(response.status).to.equal(201);     
-    });
+            expect(response.status).to.equal(201);     
+              });
+
+                it("PUT post", async ()=> {
+                const response = await sendRequest("posts/1", {
+                title: "Hi!",
+                body: "bar",
+                id: 2,
+                userId: 1}, "put");
     
+                console.log(response.data);
+                console.log(response.status);
+          
+                expect(response.status).to.equal(200);     
+                });
     
+                    it("DELETE post/1", async ()=> {
+                    const response = await sendRequest("posts/1");
+      
+                    console.log(response.data);
+                    console.log(response.status);
+      
+                    expect(response.status).to.equal(200);
+                  });
 });
 
